c8460811c11eef6b8b6581e9e18f63314a916b2a
tri.vt <tri.vt@sutrixsolutions.com>
2016-11-25 10:47:36 +0700

    update

--- js/connection.js
+++ js/connection.js
@@ -1,43 +1,39 @@
 $(document).ready(function() {
 
-  let request = indexedDB.open('customermanager', 3);
+  let request = indexedDB.open('customermanager', 1);
 
   request.onupgradeneeded = function (e) {
       var db = e.target.result;
       console.log('Upgrade...');
       if (!db.objectStoreNames.contains('customers')) {
         var os = db.createObjectStore('customers', {keyPath: 'id', autoIncrement: true});
-
         // Create Index for Name
         os.createIndex('name', 'name', {unique: false});
       }
   };
 
+  $('#frmAddMember').on('submit', function(e) {
+    addCustomer();
+  });
+
   request.onsuccess = function(e) {
-    console.log('Success Open Connection...');
     db = e.target.result;
-    db.createIndex('name', 'name', {unique: false});
     showCustomers();
   };
 
   request.onerror = function (e) {
     console.log('Error Connection');
   };
-
-  $('#frmAddMember').on('submit', function(e) {
-    addCustomer();
-  });
 });
 
 let addCustomer = () => {
-  console.log('addCustomer');
+
   let name = $('#name').val();
   let email = $('#email').val();
 
   let transaction = db.transaction(['customers'], 'readwrite');
 
   // Ask for ObjectStore
-
   let store = transaction.objectStore('customers');
 
   // Define Customer
@@ -47,19 +43,21 @@ let addCustomer = () => {
   };
 
   // Perform the Add
-
   let request = store.add(customer);
 
   //Success
   request.onsuccess = function (e) {
     window.location.href='index.html';
+    return false;
   };
 
   //Error
   request.onerror = function (e) {
     alert('SORRY, The Customer is not added');
-    console.log("Error:", e.target.error.name);
+    return false;
   };
+
+
 };
 
 
@@ -74,15 +72,35 @@ let showCustomers = () => {
       let cursor = e.target.result;
 
       if (cursor) {
-        output += '<tr>';
+        output += '<tr id="customer_'+cursor.value.id+'">';
         output += '<td> <span>' + cursor.value.id + '</span></td>';
         output += '<td> <span>' + cursor.value.name + '</span></td>';
         output += '<td> <span>' + cursor.value.email + '</span></td>';
-        output += '<td><a href="">Delete</a></td>';
+        output += '<td><a  href="#" data-delete="true">Delete</a></td>';
         output += '</tr>';
         cursor.continue();
       }
 
       $('#customer').html(output);
+  };
+};
+
+
+
+let deleteCustomer = (id) => {
+  let transaction = db.transaction(['customers'], 'readonly');
+  // Ask for ObjectStore
+  let store = transaction.objectStore('customers');
+  var request = store.delete(id);
+
+  // Success
+  request.onsuccess = function() {
+    console.log('customer' + id + ' Deleted');
+    $('.customer_' + id).remove();
+  };
+
+  request.onerror = function(e) {
+    alert('Sorry, The customer was not Removed');
+    console.log('Error', e.target.error.name);
   };
 };
